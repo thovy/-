5a75af0f138997f97299cb614b591d1cb29d8df7
Thovy <kingvely151113@gmail.com>
2022-05-15 02:11:11 +0900

    - 파일 p42583_submit.java 파일 생성 맞춘 줄 알고 만듬.
    programmers 42583 문제 풀이 중

--- PROGRAMMERS/p42583.java
+++ PROGRAMMERS/p42583.java
@@ -6,9 +6,9 @@ public class p42583 {
 
     public static void main(String[] args) {
 
-        int bridge_length = 2;      // 다리 길이, 최대로 올라갈 수 있는 트럭 수, 트럭이 다리를 지나는데 걸리는 시간.
-        int weight = 10;        // 다리가 견딜 수 있는 무게
-        int[] truck_weights = {7, 4, 5, 6};         // 트럭들의 무게
+        int bridge_length = 100;      // 다리 길이, 최대로 올라갈 수 있는 트럭 수, 트럭이 다리를 지나는데 걸리는 시간.
+        int weight = 100;        // 다리가 견딜 수 있는 무게
+        int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};         // 트럭들의 무게
         System.out.println(solution(bridge_length,weight,truck_weights));
 
     }
@@ -16,35 +16,44 @@ public class p42583 {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;     // 트럭이 다리를 모두 건너는 데 걸린 시간
 
+        // 다리에 있는 트럭
         int[] bridge = new int[bridge_length];
+        // 현재 다리 무게
 
-        // 현재 배열(다리) 전체의 합(총 무게)
-        int bridge_sum = 0;
-        for (int sum : bridge) {
-            bridge_sum += sum;
+        int t_i =0;
+        while(t_i < truck_weights.length){
+            System.out.println("-----");
+            int bridge_sum = 0;
+            for (int sum1 : bridge) {
+                bridge_sum += sum1;
+            }
+            System.out.println("무게확인1: "+bridge_sum);
+            System.out.println("다리확인1: "+Arrays.toString(bridge));
+
+            if(bridge_sum - bridge[bridge_length-1] + truck_weights[t_i] <= weight){
+                for(int i = bridge_length-2; i>=0; i--){
+                    bridge[i+1] = bridge[i];
+                }
+                bridge[0] = truck_weights[t_i];
+                System.out.println("다리확인2: "+Arrays.toString(bridge));
+                t_i++;
+                answer++;
+            }else{
+                for(int i = bridge_length-2; i>=0; i--){
+                    bridge[i+1] = bridge[i];
+                    bridge[0] = 0;
+                }
+                answer++;
+            }
+
+            for (int sum2 : bridge) {
+                bridge_sum += sum2;
+            }
+            System.out.println("다리확인3: "+Arrays.toString(bridge));
+            System.out.println("무게확인2: "+bridge_sum);
         }
-        System.out.println("다리 총 무게");
-        System.out.println(bridge_sum);
-        // 총 무게 끝
 
-        // 트럭이 다리로 건너가는 모습
-        int truck_i = 0;
-        int bridge_i= 0;
-        int i = 0;
-        while(truck_i + i < truck_weights.length){
-            int j = 0;
-
-
-            bridge[bridge_i+1] = bridge[bridge_i];
-
-            i++;
-        }
-
-
-
-
-
-
+        answer += bridge_length;
         System.out.println("걸린 시간");
         return answer;
     }
