efdc2862b5b9ff7d389b707e70c8821fa7223b0b
Thovy <kingvely151113@gmail.com>
2022-05-14 14:13:38 +0900

    - 파일 programmers 42583 문제 풀이 시작 p42563.java 파일 생성
    - README
    풀이 시작 시간 작성

---
+++ PROGRAMMERS/p42583.java
@@ -0,0 +1,17 @@
+package PROGRAMMERS;
+
+public class p42583 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+}
