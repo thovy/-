3be03c8f569e6199814ad36d6f8f898f76d141b3
Thovy <kingvely151113@gmail.com>
2022-05-17 22:56:01 +0900

    - 파일 p42840.java 파일 추가
    - README.md
    4 풀이 시작

---
+++ PROGRAMMERS/p42840.java
@@ -0,0 +1,16 @@
+package PROGRAMMERS;
+
+import java.util.Arrays;
+
+public class p42840 {
+    public static void main(String[] args) {
+        int[] answers = {1, 3, 2, 4, 2};
+        System.out.println(Arrays.toString(solution(answers)));
+
+    }
+
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
