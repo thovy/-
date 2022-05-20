5d6f07777bd57b268b67bc0686a553fb91fe2fd8
Thovy <kingvely151113@gmail.com>
2022-05-20 23:08:55 +0900

    - 폴더 BAEKJOON 폴더 생성
    - 파일
    b1000.java 생성 및 입력 완료(백준은 submit 필요없을 듯)
    
    - README.md
    6 문제 풀이 시작, 완료

---
+++ BAEKJOON/b1000.java
@@ -0,0 +1,17 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class b1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+    }
+
+//    메모리 17720 KB
+//    시간 204 ms
+//    코드길이 231 B
+}
