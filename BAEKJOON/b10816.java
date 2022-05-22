cbbd6d945b1d4f96e6ee81fd658c0e0e048a96d4
Thovy <kingvely151113@gmail.com>
2022-05-22 19:26:36 +0900

    :memo: docs - add b10816.java :memo: update No.10 in 'README.md'

---
+++ BAEKJOON/b10816.java
@@ -0,0 +1,23 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class b10816 {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader 를 사용하기 위해서는 IOException 을 던져줘야한단다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 카드 갯수 N
+
+        // 카드에 적힌 숫자 number
+
+        // 찾아볼 수 M
+
+        // 검토해야할 M 개의 숫자들 matchingNumber
+
+        // number 에 matchingNumber 가 몇 개가 있는지 나타낼 count ( 공백으로 구분해 출력 )
+    }
+}
