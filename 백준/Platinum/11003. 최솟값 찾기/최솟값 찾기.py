25e35ecf61973ae4e9a274fec67b743a3f342685
thovy <98632452+thovy@users.noreply.github.com>
2023-02-22 11:29:15 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 7552 ms, Memory: 754936 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -0,0 +1,20 @@
+from collections import deque
+import sys
+N, L = map(int, sys.stdin.readline().split())
+A = list(map(int, sys.stdin.readline().split()))
+
+answerdeque = deque()
+
+for i in range(N):
+    # answerdeque 의 뒤에서부터 0까지 A[i] 의 값보다 크면
+    while answerdeque and answerdeque[-1][0] > A[i]:
+        answerdeque.pop()    # 맨 마지막 값 뒤로 빼내기.
+        # pop()- 마지막 값 뒤로 빼내기, popleft()- 첫 값 앞으로 빼내기
+        # append()- 마지막 값으로 넣기, appendleft()- 첫 값으로 넣기
+    # 마지막값 빼낸 뒤 현재 값 A[i] 넣기
+    answerdeque.append((A[i], i))    # deque 에 인덱스 값 넣기
+    if answerdeque[0][1] <= i - L:    # 범위를 벗어나면
+        answerdeque.popleft()        # 첫번째 값 제거
+    print(answerdeque[0][0], end=" ")
+        
+    
