37bd07759280a7ad9c12893693fed613dd09cb61
thovy <98632452+thovy@users.noreply.github.com>
2023-05-30 10:17:06 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = 0
+
+    sunsu = []
+    for p in permutations(dungeons):
+        sunsu.append(p)
+    # print(sunsu)
+
+    max_go_in = 0
+    for s in sunsu:
+        pirodo = k
+        go_in = 0
+        for dungeon in s:
+            # print(pirodo, dungeon)
+            neccesary_k = dungeon[0]
+            waste_k = dungeon[1]
+            if neccesary_k <= pirodo:
+                pirodo -= waste_k
+                go_in += 1
+            else:
+                break
+
+        if go_in > max_go_in:
+            max_go_in = go_in
+
+
+
+
+    return max_go_in
